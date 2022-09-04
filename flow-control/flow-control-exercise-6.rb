46126a4c621b33eaf93f7e12955451c07f16da03
DMK-2000 <112269256+DMK-2000@users.noreply.github.com>
2022-09-04 11:17:57 -0500

    Add files via upload

---
+++ flow-control/flow-control-exercise-6.rb
@@ -0,0 +1,6 @@
+#(32 * 4) >= "129", evaluates to false
+#847 == '874', evaluates to false
+#'847' < '846', evaluates to false
+#'847' > '846', evaluates to true 
+#'847' > '8478', evaluates to false
+#'847' < '8478', evaluates to true
