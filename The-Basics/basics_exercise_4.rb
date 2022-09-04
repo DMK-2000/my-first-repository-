a7ff6a5d944c4d95e0bc69265d635f7d1c900b5b
DMK-2000 <112269256+DMK-2000@users.noreply.github.com>
2022-09-04 11:19:25 -0500

    Delete The-Basics directory

--- The-Basics/basics_exercise_4.rb
+++
@@ -1,9 +0,0 @@
-run_times = ['2009-2010','1998-1999','2006-2007','2007-2017','2006-2014']
-
-puts run_times[0]
-puts run_times[1]
-puts run_times[2]
-puts run_times[3]
-puts run_times[4]
-
-#I had to to turn the run times into strings here too
--- The-Basics/basics_exercise_7.rb
+++
@@ -1,3 +0,0 @@
-#SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
-
-#The error is saying that you need a closing curly brace.
