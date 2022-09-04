7a2729aec1b1ce051d6e856052e60dce0972c6e5
DMK-2000 <112269256+DMK-2000@users.noreply.github.com>
2022-09-04 11:17:28 -0500

    Add files via upload

---
+++ methods/methods-exercise-1.rb
@@ -0,0 +1,6 @@
+def greeting(name)
+  puts "Hello, " + name + " it's nice to meet you."
+end
+
+puts greeting("Michael")
+
---
+++ methods/methods-exercise-3.rb
@@ -0,0 +1,5 @@
+def multiply(num_1,num_2)
+  num_3 = num_1 * num_2  
+end 
+
+puts multiply(24,26)
