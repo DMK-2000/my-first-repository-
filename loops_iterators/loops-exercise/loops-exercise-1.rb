cf5825032cc287aef595cd5174be889b7dc37488
DMK-2000 <112269256+DMK-2000@users.noreply.github.com>
2022-09-04 11:17:41 -0500

    Add files via upload

---
+++ loops_iterators/loops-exercise/loops-exercise-1.rb
@@ -0,0 +1,8 @@
+# Loops exercise 1
+x = [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1
+end
+puts x
+
+# The code returns 1, 2, 3, 4, 5
---
+++ loops_iterators/loops-practice/loops-practice-4.rb
@@ -0,0 +1,14 @@
+# do_while_loops
+
+loop do
+  puts "Do you want to do that again?"
+  answer = gets.chomp
+  if answer != 'Y'
+    break 
+  end
+end
+
+begin 
+  puts "Do you want to do that again?"
+  answer = gets.chomp
+end while answer == 'Y'
