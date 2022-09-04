144f5771b96e73cb67749e07ecddb2cffd43f0a0
DMK-2000 <112269256+DMK-2000@users.noreply.github.com>
2022-09-04 11:11:05 -0500

    Add files via upload

---
+++ Flow-Control/flow-control-exercise-5.rb
@@ -0,0 +1,9 @@
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+equal_to_four(5)
+# There needs to be an end to close off the if statement and an end to close oof the method.
