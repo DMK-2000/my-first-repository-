ddb0f1e62706d1730744b804374202d59a6ebe2a
DMK-2000 <112269256+DMK-2000@users.noreply.github.com>
2022-09-04 11:19:40 -0500

    Delete Variables directory

--- Variables/var-exercise-1.rb
+++
@@ -1,4 +0,0 @@
-#name.rb
-puts "What is your name?"
-name = gets.chomp
-puts "Hello " + name
--- Variables/var-exercise-3.rb
+++
@@ -1,7 +0,0 @@
-#name_2.rb
-puts "What is your name?"
-name = gets.chomp
-
-10.times do
-  puts name
-end
