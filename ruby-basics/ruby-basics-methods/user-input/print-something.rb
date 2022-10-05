2f839bab5c2ae1ca9b36500a6abf5bb540cb3d1d
DMK-2000 <112269256+DMK-2000@users.noreply.github.com>
2022-10-05 10:41:58 -0500

    Add files via upload

---
+++ ruby-basics/ruby-basics-methods/user-input/print-something.rb
@@ -0,0 +1,9 @@
+puts "Do you want me to print something?"
+answer = gets.chomp
+
+if answer == 'y'
+  puts "Something"
+else
+  puts "End program"
+end
+
---
+++ ruby-basics/ruby-basics-methods/user-input/your-age-in-months.rb
@@ -0,0 +1,5 @@
+puts "What is your age in years?"
+age_years = gets.chomp.to_i
+age_months = age_years * 12
+puts "Your age in months is: #{age_months}"
+
